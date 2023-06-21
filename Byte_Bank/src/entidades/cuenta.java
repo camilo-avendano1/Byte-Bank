12bea830486bfdb5e57567be751343a2ae1fb0d1
camilo av <camilo.avendano1@udea.edu.co>
2023-06-21 14:11:40 -0500

    First commit

---
+++ Byte_Bank/src/entidades/cuenta.java
@@ -0,0 +1,13 @@
+package entidades;
+//cuenta:
+//    saldo
+//    agencia
+//    numero
+//    titular
+public class cuenta {
+    double saldo;
+    int agencia;
+    int numero;
+    String titular;
+
+}
